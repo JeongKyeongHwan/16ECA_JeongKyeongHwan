558850af8e7221cd4546bbc1cde59d94352c68ca
JeongKyeongHwan <dhqj12137@gmail.com>
2016-09-06 16:12:12 +0900

    added output fomat.py

---
+++ lab 01 intro/output fomat.py
@@ -0,0 +1,12 @@
+import math
+
+print("1234567890" * 4)
+print("%d" % (math.pi))
+print("%5d" % (math.pi))
+print("%5f" % (math.pi))
+print("%8f" % (math.pi))
+print("%*8f" % (math.pi))
+
+print("%16.8f" % (math.pi))
+print("123456789" * 4)
+
