c07d7f8f79a4794243da8a1e208d7cd6e887fb35
JeongKyeongHwan <dhqj12137@gmaill.com>
2016-09-20 16:54:53 +0900

    added 1~4th week 과제&재료역학 공식

---
+++ 과제/4th_week_assignment - log.py
@@ -0,0 +1,15 @@
+#-*- coding: utf8 -*-
+
+import math
+
+result = math.log10(2013114036 + 0.1)
+
+print "result = %.15e" % result
+
+result2 = math.log(2013114036 + 0.1, 2)
+
+print "result = %.15e" % result2
+
+result3 = math.log(2013114036 + 0.1)
+
+print "result = %.15e" % result3
