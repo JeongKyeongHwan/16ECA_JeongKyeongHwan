b8b2474936a1931e469b95af7cd0698515a41584
JeongKyeongHwan <dhqj12137@gmail.com>
2016-11-12 13:03:09 +0900

    added matrix.py

---
+++ lab_08_linear_algebra/test_matrix.py
@@ -0,0 +1,17 @@
+#-*- coding: utf8
+
+
+"""
+행렬 모듈을 위한 단위 테스트
+프로그램이 의도한 대로 작동하는지 확인
+
+시작하기전 PyCharm 설정
+1. file/settings
+2. Tools 메뉴 아래에서 Python integrated Tools
+3. Default test runner로 unittest선택
+4. 해동 소스 코드를 담은 폴더를 마우스 우클릭
+5. 아래쪽 "Mark Directory as"선택
+6. Sources Root
+"""
+
+import unittest
